cd25cd7cbfb89fd842e457569963ce7e3e0bfef7
Brian Anderson <andersrb@gmail.com>
2023-08-28 21:52:20 -0600

    .

--- crates/clippy-control-cli/src/main.rs
+++ crates/clippy-control-cli/src/main.rs
@@ -1,6 +1,9 @@
+#![allow(unused)]
+
 use rx::prelude::*;
 use rx::clap::{self, Parser as _};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::collections::HashMap;
 
 fn main() -> AnyResult<()> {
     rx::extras::init();
@@ -43,10 +46,37 @@ impl Cli {
 }
 
 impl CheckCommand {
-    fn run(&self, _args: &Args) -> AnyResult<()> {
-        // todo copy this to rustx template
+    fn run(&self, args: &Args) -> AnyResult<()> {
         info!("hello world");
+
+        let config = load_config(&args.config_path)?;
+        run_clippy(&config)?;
 
         Ok(())
     }
+}
+
+struct Config {
+    settings: HashMap<String, LintSetting>,
+}
+
+enum LintSetting {
+    Warn, Allow, Deny, Forbid,
+}
+
+fn load_config(path: &Path) -> AnyResult<Config> {
+    use rx::toml;
+    use rx::toml::Value;
+    use rx::toml::map::Map;
+    use rx::serde::Deserialize;
+
+    let buf = std::fs::read_to_string(path)
+        .context(format!("unable to read config file {}", path.display()))?;
+    let settings: Map<String, Value> = toml::from_str(&buf)?;
+
+    todo!()
+}
+
+fn run_clippy(config: &Config) -> AnyResult<()> {
+    todo!()
 }
